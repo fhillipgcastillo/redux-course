8942056cda9880b4844bf14efc1a102b5942d421
Ing. Fhillip Castillo <gerson.fhillip@gmail.com>
2018-02-06 22:56:20 -0400

    add reducer and load data from store

--- src/index.js
+++ src/index.js
@@ -3,14 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import store from "./store";
 
-const state = {
-  todos: [
-    {id:1, name:"Create Static UI", isComplete: true},
-    {id:2, name:"Create Initial State", isComplete: true},
-    {id:3, name:"Use State to render UI", isComplete: true},
-  ]
-};
 
-ReactDOM.render(<App todos={state.todos}/>, document.getElementById('root'));
+const state = store.getState();
+
+ReactDOM.render(<App {...state}/>, document.getElementById('root'));
 registerServiceWorker();
--- src/reducers/todo.js
+++ src/reducers/todo.js
@@ -1,15 +1,16 @@
 const initialState = {
-  todos: []
-}
+  todos: [
+    {id:1, name:"Create a Store", isComplete: true},
+    {id:2, name:"Load State through the store", isComplete: true},
+    {id:3, name:"handle state changes with redux", isComplete: false},
+  ]
+};
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case "TODO_ADD":
-      return {...state, todos: state.todos.concat(action.payload)}
-      break;
+      return {...state, todos: state.todos.concat(action.payload)};
     default:
       return state
-
   }
-  return state;
 }
