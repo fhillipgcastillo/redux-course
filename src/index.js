88ffcc8e73e7998bc92193a109c855f0ec633762
Ing. Fhillip Castillo <gerson.fhillip@gmail.com>
2018-02-06 22:02:16 -0400

    add basic todo list and start adding reducers

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+const state = {
+  todos: [
+    {id:1, name:"Create Static UI", isComplete: true},
+    {id:2, name:"Create Initial State", isComplete: true},
+    {id:3, name:"Use State to render UI", isComplete: true},
+  ]
+};
+
+ReactDOM.render(<App todos={state.todos}/>, document.getElementById('root'));
+registerServiceWorker();
