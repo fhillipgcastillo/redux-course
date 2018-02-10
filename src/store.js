88af6b9df5973bdf913be42c9a09992ac705c999
Ing. Fhillip Castillo <gerson.fhillip@gmail.com>
2018-02-09 22:29:10 -0400

    add redux devtool extension

--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
 import { createStore, applyMiddleware } from "redux";
+import { composeWithDevtools } from 'redux-devtools-extension';
 import reducer from "./reducers/todo";
 import thunk from 'redux-thunk';
 
 export default createStore(
   reducer,
-  applyMiddleware(thunk)
+  composeWithDevtools(
+    applyMiddleware(thunk)
+  )
 );
