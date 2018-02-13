8fe64335b5a8b59ad2bfc28ccd0c2f865a2f4932
Ing. Fhillip Castillo <gerson.fhillip@gmail.com>
2018-02-12 23:50:07 -0400

    fix issue with redux devtools implementation

--- src/store.js
+++ src/store.js
@@ -1,11 +1,11 @@
-import { createStore, applyMiddleware } from "redux";
-import { composeWithDevtools } from 'redux-devtools-extension';
 import reducer from "./reducers/todo";
+import { createStore, applyMiddleware } from "redux";
 import thunk from 'redux-thunk';
+// import { composeWithDevtools } from 'redux-devtools-extension';
 
 export default createStore(
   reducer,
-  composeWithDevtools(
+  // composeWithDevtools(
     applyMiddleware(thunk)
-  )
+  // )
 );
